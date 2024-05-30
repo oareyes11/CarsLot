026e310134c944d51ad905bd2cb2342ccb5f7629
oreyes <oareyesdeleon11@gmail.com>
2024-05-30 10:23:28 -0600

    :construction:

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+147dc9bb1c0dede81fb883d0615665a2fd2b6161")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54e5fb7786642830f3dfcf5e9eb3e75499e0266a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
